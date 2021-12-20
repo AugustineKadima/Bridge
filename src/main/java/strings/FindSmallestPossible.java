83f358624dfbe3f98b1a085b9e7888e99413e115
AugustineKadima <kadimasam@gmail.com>
2021-12-21 00:17:50 +0300

    Add find smallest possible number

---
+++ src/main/java/strings/FindSmallestPossible.java
@@ -0,0 +1,61 @@
+package strings;
+
+import java.util.Stack;
+
+public class FindSmallestPossible {
+    public static  int smallestPossible(int num, int k){
+
+        if(num == 0 || k > num) return -1;
+        char[] numsArray = String.valueOf(num).toCharArray();
+        Stack<Integer> numStack = new Stack<>();
+
+        for(int i = 0; i < numsArray.length; i++){
+            int j = Integer.parseInt(Character.toString(numsArray[i]));
+            if(numStack.isEmpty() || numStack.peek() <= j){
+                numStack.push(j);
+
+            }else {
+                if(k > 0){
+                    numStack.pop();
+                    numStack.push(j);
+                    k--;
+                }
+
+            }
+        }
+
+        System.out.println("Numstack: "+numStack);
+
+
+        int temp = 0;
+        for(int i = 0; i <= numStack.size(); i++){
+            if(!numStack.isEmpty()) temp = (temp* 10) + numStack.pop() ;
+        }
+
+        temp = (temp* 10) + numStack.pop();
+
+
+        int output = 0;
+        for(int i = 0; i <= temp; i++){
+            output = (output*10) + temp % 10;
+            temp = temp/10;
+        }
+
+
+
+        return output;
+    }
+    public static void main(String[] args) {
+        int results = FindSmallestPossible.smallestPossible(1432219,3);
+        System.out.println("Results: "+results);
+    }
+}
+//Given num = 1432219 and k = 3, output = 1219
+//Pseudocode
+//------------
+//Use a stack
+//Loop through nums
+//add nums[i] to stack
+//if peek of stack is larger than nums[i], pop from stack , and nums[i]
+//pop from stack and store in results
+//Return results
